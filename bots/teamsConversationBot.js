734d3ca14042629320d78c9504c0b761f15c3644
Mike Carter <MCarter@ITSRND.COM>
2019-12-03 14:27:33 -0800

    changes

--- bots/teamsConversationBot.js
+++ bots/teamsConversationBot.js
@@ -422,6 +422,8 @@ class TeamsConversationBot extends TeamsActivityHandler {
 
                   //  console.log(self.state.termArray)
 
+                  await context.sendActivity({ attachments: [this.dialogHelper.createGlossaryCard("this.state.termArray[0].definedby", "this.state.termArray[0].glossaryterm", "this.state.termArray[0].description", "this.state.termArray[0].definedby", "this.state.termArray[0].output", "this.state.termArray[0].related")] });
+
                     if (this.state.termArray.length > 0){
 
                       await context.sendActivity({ attachments: [this.dialogHelper.createBotCard('...I Found ' + this.state.termArray.length + ' Glossary Terms ','Here are the Results')] });
@@ -436,7 +438,7 @@ class TeamsConversationBot extends TeamsActivityHandler {
                       //
                       // }, this)
 
-                      await context.sendActivity({ attachments: [this.dialogHelper.createGlossaryCard(this.state.termArray[0].definedby, this.state.termArray[0].glossaryterm, this.state.termArray[0].description, this.state.termArray[0].definedby, this.state.termArray[0].output, this.state.termArray[0].related)] });
+
 
                     //  await context.sendActivity({ attachments: attachments});
 
